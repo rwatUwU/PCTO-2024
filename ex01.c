735d8226a3833fcabe9bca49a7ca0fad8424cc44
Federico Ratini <156682765+rwatUwU@users.noreply.github.com>
2024-01-15 15:19:59 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){ 
+    printf("LAZIO MERDA\n");
+    return 0;
+}
---
+++ primoesercizio.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+int x = 723;
+float y = 9.9;
+char z = 'w';
+short u = -277;
+long v = 100000;
+double w = 9.9999;
+    printf("%d\n", x);
+    printf("%f\n", y);
+    printf("%c\n", z);
+    printf("%f, %d, %c\n", y,x,z);
+    printf("%ld\n",v);
+    printf("%d\n", u);
+    printf("%lf\n", w);
+    return 0;
+}

231ab998a325840f96abd55a3fe5de4415bc6f36
Federico Ratini <156682765+rwatUwU@users.noreply.github.com>
2024-01-17 10:13:29 +0100

    Add files via upload

--- nonoesercizio.c
+++ nonoesercizio.c
@@ -8,12 +8,15 @@ int main (){
     if (a%4==0){
         if(a%100!=0){
             printf("L'anno è bisestile.\n");
-        }else if(a%400==0){
+        }
+        else if(a%400==0){
             printf("L'anno è bisestile.\n");
-        }else{
+        }
+        else{
             printf("L'anno non è bisestile.\n");
         }
-    }else{
+    }
+    else{
         printf("L'anno non è bisestile.\n");
     }
     return 0;

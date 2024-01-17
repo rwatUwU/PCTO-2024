d2596d1fd89aabc3fced91e9899f78127315b76b
Federico Ratini <156682765+rwatUwU@users.noreply.github.com>
2024-01-17 17:02:38 +0100

    Add files via upload

---
+++ diciassettesimoesercizio.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+    int n;
+    int i=0;
+    int sum;
+    int d;
+
+    printf("Inserisci un numero:\n");
+    scanf("%d", &n);
+    d=1;
+    if(n>=0){
+        while(n>=0){
+            sum=i+n;
+            printf("La media è: %d\n", (sum/d));
+            d=d+1;
+            printf("Inserisci un altro numero:\n");
+            scanf("%d", &i);
+        }
+    }
+    else{
+        printf("Il numero é negativo.\n");
+    }
+    return 0;
+}

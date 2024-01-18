5fcdd54096289ca9e5b4bc5f2de8214287cac175
Federico Ratini <156682765+rwatUwU@users.noreply.github.com>
2024-01-18 15:26:35 +0100

    Add files via upload

---
+++ ventunesimoesercizio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    char parola[6]={'c','i','a','o','!','\0'};
+    int numerico[3]={1,2,3};
+    char libero[]="ciao a tutti";
+
+    libero[0]='C'
+    printf("%s\n", parola);
+    ammette
+    printf("%s\n", libero);
+    return 0;
+}

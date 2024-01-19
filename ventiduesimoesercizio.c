8bca39130691694cbc31b79475298c8d20a1ce20
Federico Ratini <156682765+rwatUwU@users.noreply.github.com>
2024-01-19 09:19:59 +0100

    Add files via upload

--- ventiduesimoesercizio.c
+++ ventiduesimoesercizio.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-int main (){
-    char stringa[4];
+int main(){
+    int i;
+    char stringa[100];
 
-    printf("Scrivi qualcosa in minuscolo:\n");
-    scanf("%s", stringa);
-    stringa[0]= stringa[0] - 32;
-    printf("%s\n", stringa);
-    return 0;
+    printf("Inserisci un testo in minuscolo: ");
+    printf("%s \n", stringa);
+    scanf("%[^\t\n]s", stringa);
+    i = 0;
+    while (stringa[i] != '\0'){
+        if (97 <= stringa[i] && stringa[i] <=122){
+        stringa[i] = stringa[i] - 32;
+        }
+        i = i + 1;
+    }
+    printf("%s \n", stringa);
+    return (0);
 }
